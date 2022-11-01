ed4ab39208467a48acefdfa96509b3bf09b777db
yiru <eva890706@gmail.com>
2022-11-01 20:25:11 +0800

    加入專案檔案。

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+		public string[] answer;
+		public string[] GetAns() 
+		{
+			this.answer = new string[4];
+			for (int i=0; i<answer.Length; i++)
+			{
+				int seed=Guid.NewGuid().GetHashCode();
+				var random = new Random(seed);
+				int radomNumber = random.Next(0, 10);
+				answer[i] = radomNumber.ToString();
+			}
+			return answer; //answer{1,2,3,4}
+		}
+		
+		public int IsFourNum(string input)
+		{
+			bool isInt=int.TryParse(input, out int num);
+			if (isInt==false ||input.Length!=4 )
+			{
+				throw new Exception("請輸入四個數字");
+			}
+			
+			return num;
+		}
+
+		public int[] GetResult(string[] inputArray, string[] Answer) 
+		{
+			int a=0;
+			int b=0;
+			for (int i = 0; i < inputArray.Length; i++)
+			{
+				for (int k = 0; k < Answer.Length; k++)
+				{
+					if (inputArray[i] == Answer[i])
+					{
+						a++;
+					}
+					else if (inputArray[i] == Answer[k])
+					{
+						b++;
+					}
+					
+				}
+				
+			}
+			
+			return new int[] { a, b };
+		}
+
+
+
+
+	}
+}
---
+++ Q1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace Q1
+{
+	public partial class Form1 : Form
+	{
+		private string[] answer;
+		public Form1()
+		{
+			InitializeComponent();
+
+		}
+		
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			var newGame = new Class1();
+			this.answer = newGame.GetAns();
+		}
+
+		private void buttonSend_Click(object sender, EventArgs e)
+		{
+			string input =textBoxInput.Text;
+			var newGame = new Class1();
+			newGame.answer=this.answer;
+			try
+			{
+				int inputInt = newGame.IsFourNum(input);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return;
+			}
+			
+			string[] inputArray = input.Split(new char[] {' '});
+			
+
+			int[] answer = newGame.GetResult(inputArray, this.answer);
+			textBoxResult.Text += $"{this.answer[0]}{this.answer[1]}{this.answer[2]}{this.answer[3]}\r\n{answer[0]}A{answer[1]}B\r\n";
+
+			
+		}
+
+		
+	}
+}
